17eba8c5ef431b29152f914d51621a23332b02eb
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2017-09-19 18:41:08 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyezproxy',
+      version='0.1',
+      url='https://github.com/iinuwa/pyezproxy/",
+      author='Isaiah Inuwa',
+      author_email='isaiah.inuwa@gmail.com',
+      license='MIT',
+      packages=['pyezproxy'],
+      install_requires=[
+        'requests',
+        'beautifulsoup4'
+      ],
+      zip_safe=False)
